e229d8baf2712e617be7673676aacf72182592c4
Gaurav <mgaurav0416@gmail.com>
2023-08-02 17:03:53 +0530

    latest commit

--- my_app/signals.py
+++ my_app/signals.py
@@ -5,6 +5,5 @@ from .models import Employee
 @receiver(post_save, sender=Employee)
 def employee_created(sender, instance, created, **kwargs):
     if created:
-        # Here you can perform the actions you want when a new employee is created.
-        # For example, you can log a message, send an email, or trigger any other function.
+       
         print(f"New employee created: {instance.name} (ID: {instance.id})")
